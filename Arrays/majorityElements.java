38e96c069519292eb8c3b5302cf0ecf4e4e6077c
Vishakha Singh <vishakhasingh692003@gmail.com>
2025-08-12 22:19:15 +0530

    some changes

--- Arrays/majorityElements.java
+++ Arrays/majorityElements.java
@@ -2,8 +2,9 @@ import java.util.*;
 public class majorityElements {
     public static int majorityElts(int arr[]){
         int majorityElements = arr.length/2;
-        int count = 0;
+       
         for(int i=0; i<arr.length; i++){
+            int count = 0;
             for(int j=0; j<arr.length; j++){
                 if(arr[i]==arr[j]){
                     count++;
