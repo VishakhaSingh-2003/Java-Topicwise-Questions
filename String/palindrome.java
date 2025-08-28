7c4137231b495d01aeaac64f42c4aba1028aa2ed
Vishakha Singh <vishakhasingh692003@gmail.com>
2025-08-28 23:04:30 +0530

    add new code

---
+++ String/palindrome.java
@@ -0,0 +1,18 @@
+
+package String;
+
+public class palindrome {
+  public static boolean palin(String str){
+    int n = str.length();
+    for(int i=0; i<=n/2; i++){
+        if(str.charAt(i)==str.charAt(n-1-i)){
+            return true;
+        }
+    }
+    return false;
+  }  
+  public static void main(String[] args) {
+    String str = "racecar";
+    System.out.println(palin(str));
+  }
+}
