dabb99d7481fe50a3da9f2aefc2f44f475446737
Vishakha Singh <vishakhasingh692003@gmail.com>
2025-08-28 20:43:24 +0530

    add new code

--- Arrays/kadanes.java
+++ Arrays/kadanes.java
@@ -2,12 +2,13 @@ public class kadanes {
 
     public static int kadanesAlgo(int [] arr){
         int cs = 0;
-        int ms = Integer.MIN_VALUE;
+        int ms = 0;
         for(int i=0; i<arr.length; i++){
-            cs = cs+arr[i];
-            if(cs<0){
-                cs=0;
-            }
+            // cs = cs+arr[i];
+            // if(cs<0){
+            //     cs=0;
+            // }
+            cs = Math.max(arr[i], cs+arr[i]);
             ms = Math.max(ms, cs);
         }
         return ms;
