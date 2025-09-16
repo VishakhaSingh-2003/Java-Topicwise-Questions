fa4a4097188f6a47c0682557b52c505848ba6cc2
Vishakha Singh <vishakhasingh692003@gmail.com>
2025-09-16 17:23:03 +0530

    Add new code

---
+++ hashmap/iterationHashMap.java
@@ -0,0 +1,20 @@
+. package hashmap;
+import java.util.*;
+public class iterationHashMap {
+    public static void main(String[] args) {
+        HashMap<String, Integer> hm = new HashMap<>();
+        hm.put("India", 100);
+        hm.put("China", 150);
+        hm.put("Indonesia", 200);
+        hm.put("USA", 250);
+
+        Set<String> keys = hm.keySet();
+        System.out.println(keys);
+        for (String k : keys) {
+            System.out.println("key = "+ k +", value = " +hm.get(k));
+            
+        }
+
+    }
+    
+}
