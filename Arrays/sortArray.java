00ad3e5bd87e28f90582176b63a2b1904483fd37
Vishakha Singh <vishakhasingh692003@gmail.com>
2025-09-27 00:00:01 +0530

    add new code

---
+++ Arrays/sortArray.java
@@ -0,0 +1,16 @@
+public class sortArray {
+    public static boolean isSort(int arr[]){
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i]>arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int arr1[] = {1,2,3,4,5,6};
+        int arr2[] = {2,1,0,5,6};
+        System.out.println(isSort(arr1));
+        System.out.println(isSort(arr2));
+}
+}
