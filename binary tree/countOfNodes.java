00a8e84b10c66ea06f0dd5e57e57c3b51d34d71b
Vishakha Singh <vishakhasingh692003@gmail.com>
2026-03-26 14:14:24 +0530

    initial commit

---
+++ binary tree/countOfNodes.java
@@ -0,0 +1,35 @@
+public class countOfNodes {
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int count(Node root) {
+        if(root == null){
+            return 0;
+        }
+        int lCount = count(root.left);
+        int rCount = count(root.right);
+        return lCount+rCount+1;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        root.left.left.left = new Node(8);
+        root.left.left.right = new Node(9);
+        System.out.println(count(root));
+
+    }
+}
