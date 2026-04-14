126679a242fb80891e673afec7000d97f0718d23
Vishakha Singh <vishakhasingh692003@gmail.com>
2026-04-14 23:53:38 +0530

    new code

---
+++ hashmap/Treehm.java
@@ -0,0 +1,13 @@
+
+import java.util.TreeMap;
+
+public class Treehm {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> thm = new TreeMap<>();
+        thm.put("India", 100);
+        thm.put("China", 150);
+        thm.put("US", 50);
+        thm.put("Australia", 170);
+        System.out.println(thm);
+    }
+}
