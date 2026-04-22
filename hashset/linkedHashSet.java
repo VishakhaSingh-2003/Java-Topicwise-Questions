f342128f767d1a6307da018db176efa7afe0fe5d
Vishakha Singh <vishakhasingh692003@gmail.com>
2026-04-22 17:53:18 +0530

    new commit

---
+++ hashset/linkedHashSet.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class linkedHashSet {
+    
+    public static void main(String[] args) {
+        LinkedHashSet<String> cities = new LinkedHashSet<>();
+        cities.add("delhi");
+        cities.add("mumbai");
+        cities.add("nagpur");
+        cities.add("noida");
+        System.out.println(cities);
+}
+}
---
+++ hashset/treeSet.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class treeSet {
+    public static void main(String[] args) {
+      TreeSet<String> cities = new TreeSet<>();
+        cities.add("delhi");
+        cities.add("mumbai");
+        cities.add("nagpur");
+        cities.add("noida");
+
+        System.out.println(cities);
+}
+}
+  
